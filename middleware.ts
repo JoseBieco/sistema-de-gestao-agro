e3be937010e025be29124fd97bd37fd394534326
José Bieco <jo_bieco@hotmail.com>
2025-12-09 09:26:23 -0300

    fix(middleware.ts): adicionada a rota `register`  na lista de "liberação" do middleware

--- middleware.ts
+++ middleware.ts
@@ -43,7 +43,8 @@ export async function middleware(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/register")
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
